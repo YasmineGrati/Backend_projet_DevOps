a6f761c5dc28a35c44014f36a1d0c1e194441659
YasmineGrati <yasmine.grati@esprit.tn>
2022-11-09 20:42:32 +0100

    Update Facture

--- src/main/java/com/esprit/examen/services/FactureServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FactureServiceImpl.java
@@ -1,21 +1,16 @@
 package com.esprit.examen.services;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Set;
+
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.esprit.examen.entities.DetailFacture;
+
 import com.esprit.examen.entities.Facture;
-import com.esprit.examen.entities.Fournisseur;
-import com.esprit.examen.entities.Operateur;
-import com.esprit.examen.entities.Produit;
+
 import com.esprit.examen.repositories.DetailFactureRepository;
 import com.esprit.examen.repositories.FactureRepository;
-import com.esprit.examen.repositories.FournisseurRepository;
-import com.esprit.examen.repositories.OperateurRepository;
-import com.esprit.examen.repositories.ProduitRepository;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -44,7 +39,7 @@ public class FactureServiceImpl implements IFactureService {
 	@Override
 	public void cancelFacture(Long factureId) {
 		// Méthode 01
-		Facture facture = factureRepository.findById(factureId).orElse(new Facture());
+		Facture facture = factureRepository.findById(factureId).orElse(new Facture(factureId, factureId, null));
 		facture.setArchivee(true);
 		factureRepository.save(facture);
 		//Méthode 02 (Avec JPQL)
